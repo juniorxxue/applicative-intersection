92ecd35347815b21a3c7d2773eb5c7abb377000d
Xu Xue <juniorxxue@gmail.com>
2020-11-24 18:43:39 +0800

    redex model

---
+++ stlc.rkt
@@ -0,0 +1,44 @@
+#lang racket
+(require redex)
+(define-language AppL
+  (e ::=
+     n ;; int
+     x ;; variable
+     (λ x e) ;; abstraction
+     (e e) ;; application
+     (e ◇ e) ;; merge
+     (x τ) ;; annotation
+     )
+  (n ::=
+     number)
+  (A B C D::=
+     Int ;; int type
+     Top ;; top type
+     (A → B) ;; function type
+     (A & B) ;; intersection type
+     )
+  (x ::= variable-not-otherwise-mentioned))
+
+(define-judgment-form AppL
+  #:mode (subtype I O)
+  ;; #:contract (subtype A B)
+  [--------------- sub_int
+   (subtype Int Int)]
+  [--------------- sub_top
+   (subtype A Top)]
+  [(subtype C A)
+   (subtype B D)
+   ------------------------ sub_arrow
+   (subtype (A → B) (C → D))]
+  [(subtype A B)
+   (subtype A C)
+   --------------------- sub_and
+   (subtype A (B & C))]
+  [(subtype A C)
+   --------------------- sub_andl
+   (subtype (A & B) C)]
+  [(subtype B C)
+   --------------------- sub_andr
+   (subtype (A & B) C)])
+   
+     
